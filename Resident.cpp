db8dfaa917734a780d84992444fcd2f470fdb179
mihaibianca24 <bm24082006@gmail.com>
2026-05-19 17:44:45 +0300

    Added class Resident

---
+++ Resident.cpp
@@ -0,0 +1,92 @@
+//
+// Created by Bianca Mihai on 19.05.2026.
+//
+
+#include "Resident.h"
+#include "Entity.h"
+#include <algorithm>
+
+Resident :: Resident() : Entity("Unknown"){
+    this->energy=100;
+    this->hunger=100;
+    this->happiness=100;
+    this->hygiene=100;
+    this->money=500;
+}
+Resident :: Resident(const std::string &name) : Entity(name){
+    this->energy=100;
+    this->hunger=100;
+    this->happiness=100;
+    this->hygiene=100;
+    this->money=500;
+}
+Resident :: Resident(const Resident &resident) : Entity(resident){
+    this->energy=resident.energy;
+    this->hunger=resident.hunger;
+    this->happiness=resident.happiness;
+    this->hygiene=resident.hygiene;
+    this->money=resident.money;
+}
+Resident& Resident::operator=(const Resident& other) {
+    if (this != &other) {
+        Entity::operator=(other);
+        energy = other.energy;
+        hunger = other.hunger;
+        happiness = other.happiness;
+        hygiene = other.hygiene;
+        money = other.money;
+    }
+    return *this;
+}
+Resident::~Resident() = default;
+
+int Resident::getEnergy() const { return energy; }
+int Resident::getHunger() const { return hunger; }
+int Resident::getHappiness() const { return happiness; }
+int Resident::getHygiene() const { return hygiene; }
+int Resident::getMoney() const { return money; }
+
+void Resident::modifyEnergy(int amount) {
+    energy = std::clamp(energy + amount, 0, 100);
+}
+void Resident::modifyHunger(int amount) {
+    hunger = std::clamp(hunger + amount, 0, 100);
+}
+void Resident::modifyHappiness(int amount) {
+    happiness = std::clamp(happiness + amount, 0, 100);
+}
+void Resident::modifyHygiene(int amount) {
+    hygiene = std::clamp(hygiene + amount, 0, 100);
+}
+
+void Resident::addMoney(int amount) {
+    money += amount;
+}
+
+bool Resident::spendMoney(int amount) {
+    if (money >= amount) {
+        money -= amount;
+        return true;
+    }
+    return false;
+}
+
+void Resident::update(int deltaSeconds) {
+    modifyHunger(-1 * deltaSeconds);
+    modifyHygiene(-1 * deltaSeconds);
+    modifyEnergy(-1 * deltaSeconds);
+    modifyHappiness(-1 * deltaSeconds);
+}
+
+std::string Resident::getDescription() const {
+    return getName() +
+           " | Energy: "    + std::to_string(energy) +
+           " | Hunger: "    + std::to_string(hunger) +
+           " | Happiness: " + std::to_string(happiness) +
+           " | Hygiene: "   + std::to_string(hygiene) +
+           " | Money: "     + std::to_string(money);
+}
+
+bool Resident::isDead() const {
+    return energy <= 0;
+}
---
+++ Resident.h
@@ -0,0 +1,43 @@
+//
+// Created by Bianca Mihai on 19.05.2026.
+//
+
+#ifndef DREAM_APARTMENT_SIMULATOR_RESIDENT_H
+#define DREAM_APARTMENT_SIMULATOR_RESIDENT_H
+#include "Entity.h"
+
+class Resident : public Entity {
+    private:
+    int energy;
+    int hunger;
+    int happiness;
+    int hygiene;
+    int money;
+
+public:
+    Resident();
+    Resident(const std::string& name);
+    Resident(const Resident &resident);
+    Resident & operator=(const Resident &resident);
+    ~Resident();
+
+    int getEnergy() const;
+    int getHunger() const;
+    int getHappiness() const;
+    int getHygiene() const;
+    int getMoney() const;
+
+    void modifyEnergy(int amount);
+    void modifyHunger(int amount);
+    void modifyHappiness(int amount);
+    void modifyHygiene(int amount);
+    void addMoney(int amount);
+    bool spendMoney(int amount); // returneaza false daca nu are destui bani
+
+    void update(int deltaSeconds) override;
+    std::string getDescription() const override;
+    bool isDead() const;
+};
+
+
+#endif //DREAM_APARTMENT_SIMULATOR_RESIDENT_H
